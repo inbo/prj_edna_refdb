1c8922652f267fc166348f88ce6743473ea9ca75
nick-dil <nick.dillen@inbo.be>
2025-05-19 10:26:16 +0200

    rclone mount of input data GDrive folder

--- hpc_scripts/00_refdb_init.R
+++ hpc_scripts/00_refdb_init.R
@@ -1,5 +1,9 @@
 # Adapted From: https://github.com/inbo/prj_edna_refdb
 
+# library(RSQLite)
+library(tidyverse)
+library(googlesheets4)
+
 # Load source
 VSC_DATA_dir = Sys.getenv("VSC_DATA")
 
@@ -8,35 +12,40 @@ VSC_DATA_dir = Sys.getenv("VSC_DATA")
 ###-------------------------
 #google drive gebruikersnaam
 user_name <- "nick.dillen@inbo.be"
-
-#shortcut naar google drive path van het project
-root_gdrive <- file.path("G:", 
-                         ".shortcut-targets-by-id",
-                         "0B1XJuciaZSENZG55ZnlDQ0FvT0E", 
-                         "PRJ_eDNA",
-                         "PRJ_eDNA_Refdb_2023" )
-fasta_inputs_location <- file.path(root_gdrive, 'input_seqs', 'import')
-
-#google key van de sheet waar de soortenlijst, multihit, ... bewaard wordt
-metadata_gdrive_key <- "1NidznDq9EVHN4_wfrhv0W5gZ0Q-S8wEn5jvXbb9rf8k"
-
 googlesheets4::gs4_auth(user_name) #googlesheets4::gs4_deauth()
 
 ###-------------------------
+# INPUT on HPC
+## Could be made into cmd arguments
+output_folder = "/staging/leuven/stg_00184/genetic_diversity/reference_databases/PRJ_eDNA_Refdb_2025"
+taxdump_name <- file.path(output_folder, "taxonomy/2025-05-16-taxdump.tar.gz")
 
-# INPUT
-data_folder = "/staging/leuven/stg_00184/genetic_diversity/reference_databases/PRJ_eDNA_Refdb_2025"
-taxdump_name <- file.path(data_folder, "taxonomy/taxdump_2023-07-24.tar.gz")
-fasta_inputs_location <- file.path(data_folder, 'input_seqs', 'import')
-
-setwd(data_folder)
-
+# INPUT on GDRIVE
 #zelf te kiezen namen en locaties
+## This is not really necessary here, maybe split teleo and riaz so we can update and execute seperatly
+## Now we do everything x2
 db_name_teleo <- "refdb_teleo_XXX"
 db_name_riaz  <- "refdb_riaz_XXX"
 
 ### Globale variabelen
 ###----------------------
+## HARDCODED
+#shortcut naar google drive path van het project
+## MyDrive + shortcut to PROJECTEN... in a MOUNTED folder Rclone
+
+system("rclone version") # make sure rclone module is loaded in the pre-run scriplet! (module load rclone)
+system("rclone mount gdrive: ~/G_MOUNT -vv --daemon")
+
+root_gdrive <- file.path("~/G_MOUNT", 
+                         "GDRIVE_shortcuts",
+                         "PRJ_eDNA_Refdb_2023" )
+
+fasta_inputs_location <- file.path(root_gdrive, 'input_seqs', 'import')
+
+fasta_inputs_location_id = "1Q0OgyA_WSHGWdZjy3LvjAzP9MGiqBmCr"
+
+#google key van de sheet waar de soortenlijst, multihit, ... bewaard wordt
+metadata_gdrive_key <- "1NidznDq9EVHN4_wfrhv0W5gZ0Q-S8wEn5jvXbb9rf8k"
 
 cleaned_input_fasta <- file.path("database", "input", "cleaned_inputs.fasta") 
 all_input_fasta <- file.path("database", "input", "all_inputs.fasta") 
@@ -50,15 +59,12 @@ output_path_teleo <- file.path("database", db_name_teleo, "output")
 
 ### Environment klaarzetten
 ###----------------------------
-
-library(tidyverse)
-# library(googlesheets4)
-# library(RSQLite)
 source(file.path(VSC_DATA_dir, "prj_edna_refdb","scripts/_functions_fasta.R"))
 source(file.path(VSC_DATA_dir, "prj_edna_refdb","scripts/_functions_refdb.R"))
 source(file.path(VSC_DATA_dir, "prj_edna_refdb","scripts/_functions_postprocessing.R"))
 
-
+# Create output directories in target folder
+setwd(output_folder)
 if (!dir.exists("database")) dir.create("database")
 if (!dir.exists(refdb_location_riaz)) dir.create(file.path("database", db_name_riaz))
 if (!dir.exists(refdb_location_teleo)) dir.create(file.path("database", db_name_teleo))
