564ccb265095b3f688eb5f072017dedeb7b78ae5
nick-dil <nick.dillen@inbo.be>
2026-01-29 14:54:58 +0100

    assert df_inputs_all exists + rm unused vars

--- hpc_scripts/01_refdb_import.R
+++ hpc_scripts/01_refdb_import.R
@@ -1,5 +1,6 @@
 # ---------------------------
 ### HARDCODED OUTPUT NAMES ###
+getwd()
 
 # For processing of INPUT from GDrive
 import_dir_name = file.path("database", "input_reference_sequences")
@@ -20,6 +21,7 @@ cleaned_input_fasta <- file.path(
 df_species_all <- read_sheet(metadata_gdrive_key, "Soortenlijst") %>% 
   mutate(ROWNR = 1:n())
 
+# filter OK data
 df_species <- df_species_all %>% 
   filter(Taxid > 0, !is.na(Priority), Priority != 9)
 
@@ -54,17 +56,16 @@ df_inputs_orig <- df_inputs_orig %>%
 # Select duplicated records
 whidup <- which(duplicated(df_inputs_orig %>%  select(genbank_id)))
 dupids <- df_inputs_orig %>% slice(whidup) %>% pull(genbank_id)
-#df_inputs_all %>% filter(genbank_id %in% dupids) %>% arrange(genbank_id) %>% view()
+# df_inputs_all %>% filter(genbank_id %in% dupids) %>% arrange(genbank_id) %>% view()
 
 # remove
-if (length(whidup)) df_inputs_all <- df_inputs_orig[-whidup, ]
+if (length(whidup)) df_inputs_all <- df_inputs_orig[-whidup, ] else {df_inputs_all <- df_inputs_orig}
 
 
 #Kijk of er nog prioriteit-9 (soorten die niet weerhouden worden wegens bvb hybriden) aan seq_errors moet toegevoegd worden
 
 #! Haal foutieve sequenties uit 
 #---------------------------------
-
 df_inputs <- df_inputs_all %>% 
   mutate(taxid = as.numeric(taxid)) %>% 
   filter(!(genbank_id %in% (df_seq_errors %>% pull(ENTRY_ID)))) %>% 
@@ -75,14 +76,12 @@ df_inputs <- df_inputs_all %>%
 df_inputs %>% filter(taxid %in% (df_species %>% filter(Priority == 9) %>% pull(Taxid)))
 
 #find duplicate ids with different species
-dup_ids <- df_inputs %>% group_by(genbank_id) %>% 
+df_inputs %>% group_by(genbank_id) %>% 
   summarise(aantal_seqs = n(), 
             sources = paste(source, collapse = ","),
             aantal_taxa = n_distinct(taxid), 
             taxa = paste(unique(taxid), collapse = ",")) %>% 
   filter(aantal_seqs > 1 & aantal_taxa > 1)
-
-
 
 #! Bewaar de (gecleande) data
 #-------------------------------
